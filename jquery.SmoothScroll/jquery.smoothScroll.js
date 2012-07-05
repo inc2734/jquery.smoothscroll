727b875aafd0d64ad17640f00e8f28c79210d175
inc2734 <inc@2inc.org>
2012-07-05 23:58:20 +0900

    消し忘れ

--- jquery.SmoothScroll/jquery.smoothScroll.js
+++ jquery.SmoothScroll/jquery.smoothScroll.js
@@ -91,14 +91,6 @@ jQuery( function( $ ) {
 		duration : 1000,
 		easing : 'easeOutQuint'
 	});
-	/*
-	$('a[href^="#"]').live( 'hover', function() {
-		$(this).SmoothScroll({
-			duration : 1000,
-			easing : 'easeOutQuint'
-		});
-	});
-	*/
 });
 
 /*
